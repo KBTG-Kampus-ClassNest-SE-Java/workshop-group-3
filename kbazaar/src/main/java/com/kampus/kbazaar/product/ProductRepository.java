a754893ff62bbeeb88029d44a1f19023aab3c4ce
bencomtech <bencomtech@gmail.com>
2024-03-24 11:06:03 +0700

    add pagination

--- kbazaar/src/main/java/com/kampus/kbazaar/product/ProductRepository.java
+++ kbazaar/src/main/java/com/kampus/kbazaar/product/ProductRepository.java
@@ -1,11 +1,11 @@
 package com.kampus.kbazaar.product;
 
 import java.util.Optional;
-import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface ProductRepository extends JpaRepository<Product, Long> {
+public interface ProductRepository extends PagingAndSortingRepository<Product, Long> {
 
     Optional<Product> findBySku(String sku);
 }
--- kbazaar/src/main/java/com/kampus/kbazaar/product/ProductResponse.java
+++ kbazaar/src/main/java/com/kampus/kbazaar/product/ProductResponse.java
@@ -1,5 +1,42 @@
 package com.kampus.kbazaar.product;
 
-import java.math.BigDecimal;
+import java.util.List;
 
-public record ProductResponse(Long id, String name, String sku, BigDecimal price, int quantity) {}
+public class ProductResponse {
+    private List<Product> products;
+    private int currentPage;
+    private int totalPages;
+    private long totalItems;
+
+    public List<Product> getProducts() {
+        return products;
+    }
+
+    public void setProducts(List<Product> products) {
+        this.products = products;
+    }
+
+    public int getCurrentPage() {
+        return currentPage;
+    }
+
+    public void setCurrentPage(int currentPage) {
+        this.currentPage = currentPage;
+    }
+
+    public int getTotalPages() {
+        return totalPages;
+    }
+
+    public void setTotalPages(int totalPages) {
+        this.totalPages = totalPages;
+    }
+
+    public long getTotalItems() {
+        return totalItems;
+    }
+
+    public void setTotalItems(long totalItems) {
+        this.totalItems = totalItems;
+    }
+}
